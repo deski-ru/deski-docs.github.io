1d10604d2d12f6589989534a39ed11091d6924b9
Roman Puzanov <puzanovrs@gmail.com>
2020-03-21 15:00:04 +0500

    Выделение текущего пункта меню

---
+++ docs/static/js/site.js
@@ -0,0 +1,14 @@
+﻿function setCurrentMenuItem() {
+    var url = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
+    var items = document.getElementsByClassName('nav-link');
+
+    for (var item of items) {
+        var href = item.getAttribute('href');
+        if (href === url) {
+            item.style.fontWeight = 'bold';
+            item.style.textDecoration = 'underline';
+        }
+    }
+}
+
+setCurrentMenuItem();
